dd8d7baccd0ae5b836534e5e67dabc371de2421b
Nerovski <j.stokowski97@gmail.com>
2019-10-23 23:07:43 +0200

    Anti Aliasing

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
 
     // Create window
     g_Window.Initialize(1920, 1080, "Rubik's cube");
