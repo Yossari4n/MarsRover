dcfc1da83fe84d85eed8bd5714857481f2fef6f3
Nerovski <j.stokowski97@gmail.com>
2019-10-30 12:42:15 +0100

    Line class

--- src/rendering/primitives/Line.cpp
+++ src/rendering/primitives/Line.cpp
@@ -1,65 +1,35 @@
-//#include "Line.h"
-//
-//Line::Line(glm::vec3 start, glm::vec3 end, glm::vec3 color)
-//    : IDrawable(ShaderProgram::EType::PureColor)
-//    , m_Start(start)
-//    , m_End(end)
-//    , m_Color(color) {
-//    SetupLine();
-//}
-//
-//Line::Line(const Line& other)
-//    : IDrawable(ShaderProgram::EType::PureColor)
-//    , m_Start(other.m_Start)
-//    , m_End(other.m_End)
-//    , m_Color(other.m_Color) {
-//    SetupLine();
-//}
-//
-//Line& Line::operator=(const Line &other) {
-//    if (this == &other) {
-//        return *this;
-//    }
-//
-//    m_Start = other.m_Start;
-//    m_End = other.m_End;
-//    m_Color = other.m_Color;
-//    m_ShaderType = EShaderType::PureColor;
-//    SetupLine();
-//
-//    return *this;
-//}
-//
-//Line::~Line() {
-//    glDeleteBuffers(1, &m_VBO);
-//    glDeleteVertexArrays(1, &m_VAO);
-//}
-//
-//void Line::Draw(const ShaderProgram &shader) const {
-//    glm::mat4 model(1.0f);
-//    shader.Uniform("model", model);
-//    shader.Uniform("color", m_Color);
-//
-//    glBindVertexArray(m_VAO);
-//    glDrawArrays(GL_LINES, 0, 6);
-//    glBindVertexArray(0);
-//}
-//
-//void Line::SetupLine() {
-//    GLfloat vertices[] = { m_Start.x, m_Start.y, m_Start.z, m_End.x, m_End.y, m_End.z };
-//    
-//    glGenVertexArrays(1, &m_VAO);
-//    glGenBuffers(1, &m_VBO);
-//
-//    glBindVertexArray(m_VAO);
-//
-//    glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-//
-//    // Position
-//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *)0);
-//    glEnableVertexAttribArray(0);
-//
-//    glBindVertexArray(0);
-//    glBindBuffer(GL_ARRAY_BUFFER, 0);
-//}
+#include "Line.h"
+
+Line::Line(glm::vec3 start, glm::vec3 end, glm::vec3 color)
+    : m_Color(color) {
+    GLfloat vertices[] = { start.x, start.y, start.z, end.x, end.y, end.z };
+
+    glGenVertexArrays(1, &m_VAO);
+    glGenBuffers(1, &m_VBO);
+
+    glBindVertexArray(m_VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+Line::~Line() {
+    glDeleteBuffers(1, &m_VBO);
+    glDeleteVertexArrays(1, &m_VAO);
+}
+
+void Line::Draw(const ShaderProgram &shader) const {
+    shader.Uniform("model", glm::mat4(1.0f));
+    shader.Uniform("color", m_Color);
+
+    glBindVertexArray(m_VAO);
+    glDrawArrays(GL_LINES, 0, 6);
+    glBindVertexArray(0);
+}
--- src/rendering/primitives/Line.h
+++ src/rendering/primitives/Line.h
@@ -1,33 +1,25 @@
 #ifndef Line_h
 #define Line_h
 
-//#include "../../rendering/IDrawable.h"
-//
-//class Line : public IDrawable {
-//public:
-//    Line(glm::vec3 start, glm::vec3 end, glm::vec3 color);
-//    Line(const Line& other);
-//    Line& operator=(const Line& other);
-//    ~Line();
-//
-//    void Draw(const ShaderProgram& shader) const override;
-//
-//    const glm::vec3& Start() const { return m_Start; }
-//
-//    const glm::vec3& End() const { return m_End; }
-//
-//    const glm::vec3& Color() const { return m_Color; }
-//    void Color(const glm::vec3 color) { m_Color = color; }
-//
-//private:
-//    glm::vec3 m_Start;
-//    glm::vec3 m_End;
-//    glm::vec3 m_Color;
-//    
-//    GLuint m_VAO;
-//    GLuint m_VBO;
-//    
-//    void SetupLine();
-//};
+#include "../../rendering/IDrawable.h"
+
+class Line : public IDrawable {
+public:
+    Line(glm::vec3 start, glm::vec3 end, glm::vec3 color);
+
+    Line() = delete;
+    Line(const Line&) = delete;
+    Line& operator=(const Line&) = delete;
+    Line(Line&&) = delete;
+    Line& operator=(Line&&) = delete;
+    ~Line();
+
+    void Draw(const ShaderProgram& shader) const override;
+
+private:
+    GLuint m_VAO;
+    GLuint m_VBO;
+    glm::vec3 m_Color;
+};
 
 #endif
